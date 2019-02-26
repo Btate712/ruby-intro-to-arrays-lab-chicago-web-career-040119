80d35129348c342463730f653ffd7d0247535f90
Robert Tate <btate712@gmail.com>
2019-02-26 04:50:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,16 @@ end
 def array_with_two_elements
   array = [1, 2]
 end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[array.length-1]
+end
+
